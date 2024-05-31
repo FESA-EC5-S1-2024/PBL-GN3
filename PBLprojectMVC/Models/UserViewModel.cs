5419deaaf9334e4dc3c2c74383fe40bf717d6f26
Rhian Rodrigues Barbosa <rhianbarbosa2003@gmail.com>
2024-05-30 23:09:34 -0300

    commit de preparação de Login
    Foram desenvolvidos e atualizados varios aspectos acerca do desenvolvimento das telas de login, registro, home e sobre o backend aspnet em geral, onde foram adicionadas as classes padrão e o direcionamento para a realização de login

---
+++ PBLprojectMVC/Models/UserViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace PBLprojectMVC.Models
+{
+    public class UserViewModel : StandardViewModel
+    {
+        public string Nome { get; set; }
+        public string Email { get; set; }
+        public string Senha { get; set; }
+        public bool IsAdmin { get; set; }
+    }
+}
