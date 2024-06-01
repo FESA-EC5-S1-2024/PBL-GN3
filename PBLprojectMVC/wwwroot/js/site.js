de17d6a2a26244f127c0eba260ce0d7bc2e26f6b
binhoo <fabiogomesc7@gmail.com>
2024-06-01 18:08:08 -0300

    Implement devices update and delete

--- PBLprojectMVC/wwwroot/js/site.js
+++ PBLprojectMVC/wwwroot/js/site.js
@@ -1,4 +1,22 @@
-﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
-
-// Write your JavaScript code.
+﻿function deleteRecord(table, id) {
+    Swal.fire({
+        title: 'Confirma a exclusão do registro?',
+        text: "Se você excluir, não será possível recuperar este registro!",
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#d33',
+        cancelButtonColor: '#3085d6',
+        confirmButtonText: 'Sim, exclua!',
+        cancelButtonText: 'Cancelar'
+    }).then((result) => {
+        if (result.isConfirmed) {
+            location.href = table + '/Delete?id=' + id;
+        } else {
+            Swal.fire(
+                'Cancelado',
+                'Seu registro está seguro :)',
+                'info'
+            );
+        }
+    });
+}
