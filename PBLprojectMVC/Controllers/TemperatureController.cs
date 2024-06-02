240bab313a5f5e989f18652e634a4eb1f3cd2499
binhoo <fabiogomesc7@gmail.com>
2024-06-02 19:07:21 -0300

    Implement 'AdvancedQuery' for Temperature

--- PBLprojectMVC/Controllers/TemperatureController.cs
+++ PBLprojectMVC/Controllers/TemperatureController.cs
@@ -1,14 +1,18 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using PBLprojectMVC.DAO;
 using PBLprojectMVC.Models;
+using PBLprojectMVC.Services;
 
 namespace PBLprojectMVC.Controllers
 {
     public class TemperatureController : StandardController<TemperatureViewModel>
     {
+        private readonly TemperatureService _temperatureService;
         public TemperatureController()
         {
             DAO = new TemperatureDAO();
+            _temperatureService = new TemperatureService();
         }
 
         protected override void ValidateData(TemperatureViewModel model, string operation)
@@ -23,5 +27,63 @@ namespace PBLprojectMVC.Controllers
                 ModelState.AddModelError("DeviceId", "Selecione um dispositivo.");
         }
 
+        public async Task<ActionResult> GetTemperaturePartial(string deviceName, DateTime dateFrom, DateTime dateTo, int hLimit, int hOffset)
+        {
+            try
+            {
+                if (hLimit <= 0)
+                    hLimit = 10;
+                string entityName = GetEntityName(deviceName);
+                List<TemperatureViewModel> temperatures = await _temperatureService.GetTemperatureValues(entityName, dateFrom, dateTo, hLimit, hOffset);
+                return PartialView("pvGridTemperatures", temperatures);
+            }
+            catch (Exception error)
+            {
+                return Json(new { error = true, msg = error.Message });
+            }
+        }
+
+        public static string GetEntityName(string input)
+        {
+            var prefix = string.Concat(input.TakeWhile(char.IsLetter));
+            var numericPart = string.Concat(input.SkipWhile(char.IsLetter));
+            if (string.IsNullOrEmpty(numericPart))
+            {
+                numericPart = "001";
+            }
+            prefix = char.ToUpper(prefix[0]) + prefix.Substring(1);
+            return $"urn:ngsi-ld:{prefix}:{numericPart}";
+        }
+
+        public IActionResult AdvancedQuery()
+        {
+            try
+            {
+                ViewBag.UserLogged = HelperController.LoginSessionVerification(HttpContext.Session);
+                ViewBag.IsAdmin = HelperController.AdminSessitionVerification(HttpContext.Session);
+                LoadDeviceListCombo();
+                return View("AdvancedQuery");
+            }
+            catch (Exception error)
+            {
+                return View("Error", new ErrorViewModel(error.Message));
+            }
+        }
+
+        private void LoadDeviceListCombo()
+        {
+            DeviceDAO deviceDAO = new DeviceDAO();
+            var devices = deviceDAO.GetAll();
+            List<SelectListItem> listDevices = new List<SelectListItem>
+            {
+                new SelectListItem("Selecione um dispositivo...", "0")
+            };
+            foreach (var dev in devices)
+            {
+                SelectListItem item = new SelectListItem(dev.Name, dev.Name);
+                listDevices.Add(item);
+            }
+            ViewBag.Devices = listDevices;
+        }
     }
 }
---
+++ PBLprojectMVC/Services/TemperatureService.cs
@@ -0,0 +1,64 @@
+﻿using Newtonsoft.Json;
+using PBLprojectMVC.Models;
+
+namespace PBLprojectMVC.Services
+{
+    public class TemperatureService
+    {
+        private readonly HttpClient _httpClient;
+        private readonly string host = "your_host";
+
+        public TemperatureService()
+        {
+            _httpClient = new HttpClient();
+        }
+
+        public async Task<List<TemperatureViewModel>> GetTemperatureValues(string entityName, DateTime dateFrom, DateTime dateTo, int hLimit, int hOffset)
+        {
+            string url = $"http://{host}:8666/STH/v2/entities/{entityName}/attrs/temperature?type=Temp&hLimit={hLimit}&hOffset={hOffset}";
+
+            if (dateFrom != default && dateTo != default)
+            {
+                url += $"&dateFrom={dateFrom:yyyy-MM-ddTHH:mm:ss.fff}&dateTo={dateTo:yyyy-MM-ddTHH:mm:ss.fff}";
+            }
+            _httpClient.DefaultRequestHeaders.Add("fiware-service", "smart");
+            _httpClient.DefaultRequestHeaders.Add("fiware-servicepath", "/");
+
+            HttpResponseMessage response = await _httpClient.GetAsync(url);
+            if (!response.IsSuccessStatusCode)
+            {
+                throw new HttpRequestException($"Error fetching temperature values: {response.StatusCode} - {response.ReasonPhrase}");
+            }
+
+            string jsonResponse = await response.Content.ReadAsStringAsync();
+            var temperatureResponse = JsonConvert.DeserializeObject<TemperatureApiResponse>(jsonResponse);
+
+            var temperatureList = new List<TemperatureViewModel>();
+            foreach (var value in temperatureResponse.Value)
+            {
+                temperatureList.Add(new TemperatureViewModel
+                {
+                    Time = value.RecvTime,
+                    Value = value.AttrValue
+                });
+            }
+
+            return temperatureList;
+        }
+    }
+
+    public class TemperatureApiResponse
+    {
+        public string Type { get; set; }
+        public List<TemperatureValue> Value { get; set; }
+    }
+
+    public class TemperatureValue
+    {
+        public string Id { get; set; }
+        public DateTime RecvTime { get; set; }
+        public string AttrName { get; set; }
+        public string AttrType { get; set; }
+        public float AttrValue { get; set; }
+    }
+}
